e3e8a47a806feb1d27d5bd62c052a83bc50e1499
AdamusBG <60892124+AdamusBG@users.noreply.github.com>
2020-03-03 18:01:09 +0000

    Works to loop through folder and convert
    Works well for the function - going to add in some final features

--- JPGtoPNGconverter.py
+++ JPGtoPNGconverter.py
@@ -2,13 +2,21 @@ import sys     #will need sys to get file paths from the command line
 import os      #os needed to manipulate files
 from PIL import Image      #needed for the final stage to manipulate the images
 
+
 filepath1 = sys.argv [1]
 filepath2 = sys.argv[2]
 
-if os.path.isdir(filepath2):
-	pass
-else:
+if not os.path.isdir(filepath2):
 	try:
 		os.mkdir(filepath2)
 	except FileNotFoundError:
-		print("Please carefully check the file path entered and verify that it is valid.")
+		print("FileNotFoundError - Please carefully check the file path entered and verify that it is valid.")
+
+for filename in os.listdir(filepath1):
+	try:
+		img = Image.open(f'{filepath1}{filename}')
+		trimname = os.path.splitext(filename)[0]
+		img.save(f'{filepath2}{trimname}.png', 'png')
+	except FileNotFoundError:
+		print("FileNotFoundError - make sure both file paths end with \'/\'")
+		break
